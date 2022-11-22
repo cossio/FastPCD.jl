1dc499b1a329ab4859ad2d66e1f2222dd3ddcacb
Jorge Fernandez-de-Cossio-Diaz <cossio@users.noreply.github.com>
2022-11-22 16:32:22 +0100

    fast pcd

---
+++ README.md
@@ -0,0 +1,20 @@
+# FastPCD Julia package
+
+[![License](https://img.shields.io/badge/license-MIT-green.svg)](https://github.com/cossio/FastPCD.jl/blob/master/LICENSE.md)
+![](https://github.com/cossio/FastPCD.jl/workflows/CI/badge.svg)
+[![codecov](https://codecov.io/gh/cossio/FastPCD.jl/branch/master/graph/badge.svg?token=O5P8LQTVF3)](https://codecov.io/gh/cossio/FastPCD.jl)
+
+FastPCD for RBMs implementation in Julia. See Tieleman et al.
+
+## Installation
+
+This package is not registered. Install with:
+
+```julia
+import Pkg
+Pkg.add("FastPCD")
+```
+
+## References
+
+Tieleman, Tijmen, and Geoffrey Hinton. "Using fast weights to improve persistent contrastive divergence." Proceedings of the 26th annual international conference on machine learning. 2009.
---
+++ src/FastPCD.jl
@@ -0,0 +1,83 @@
+module FastPCD
+
+using RestrictedBoltzmannMachines: RBM, Binary, Spin, Potts, Gaussian, ReLU, dReLU
+using Optimisers: AbstractRule, setup, update!, Adam
+
+"""
+    fpcd!(rbm, data)
+
+Trains the RBM on data using Persistent Contrastive divergence, with fast weights.
+See http://dl.acm.org/citation.cfm?id=1553374.1553506.
+"""
+function fast_pcd!(rbm::RBM, data::AbstractArray;
+    batchsize::Int = 1,
+    epochs::Int = 1,
+    optim::AbstractRule = Adam(1f-3),
+    wts::Union{AbstractVector,Nothing} = nothing,
+    steps::Int = 1,
+    optimfast::AbstractRule = Adam(1f-2), # optimizer algorithm for fast parameters (should have higher learning rate)
+    decayfast::Real = 19/20  # weight decay of fast parameters
+)
+    @assert size(data) == (size(rbm.visible)..., size(data)[end])
+    isnothing(wts) || @assert size(data)[end] == length(wts)
+
+    moments = moments_from_samples(rbm.visible, data; wts)
+    vm = sample_from_inputs(rbm.visible, falses(size(rbm.visible)..., batchsize))
+
+    rbmfast = deepcopy(rbm) # store fast parameters
+
+    ps = (; visible = rbm.visible.par, hidden = rbm.hidden.par, w = rbm.w)
+    state = setup(optim, ps)
+
+    # (Actually, the parameters of rbmfast are the sums θ_regular + θ_fast)
+    for _ in 1:epochs, (vd, wd) in minibatches(data, wts; batchsize)
+        vm = sample_v_from_v(rbmfast, vm; steps)
+
+        ∂d = ∂free_energy(rbm, vd; wts=wd, moments)
+        ∂m = ∂free_energy(rbm, vm)
+        ∂ = ∂d - ∂m
+
+        gs = (; visible=∂.visible, hidden=∂.hidden, w=∂.w)
+        state, ps = update!(state, ps, gs)
+
+        decayfast!(rbmfast, rbm; decay=decayfast)
+    end
+    return rbm
+end
+
+#= Decays parameters of `rbmfast` towards those of `rbm`.
+In other words, writing the parametrs of `rbmfast` as
+ω_regular + ω_fast, where ω_regular are the prameters of
+`rbm`, then here we decay the ω_fast part towards zero. =#
+function decayfast!(rbmfast::M, rbm::M; decay::Real) where {M<:RBM}
+    decayfast!(rbmfast.visible, rbm.visible; decay)
+    decayfast!(rbmfast.hidden, rbm.hidden; decay)
+    decayfast!(rbmfast.w, rbm.w; decay)
+end
+
+function decayfast!(fast::L, regular::L; decay::Real) where {L<:Union{Binary,Spin,Potts}}
+    @assert size(fast) == size(regular)
+    decayfast!(fast.θ, regular.θ; decay)
+end
+
+function decayfast!(fast::L, regular::L; decay::Real) where {L<:Union{Gaussian,ReLU}}
+    @assert size(fast) == size(regular)
+    decayfast!(fast.θ, regular.θ; decay)
+    decayfast!(fast.γ, regular.γ; decay)
+end
+
+function decayfast!(fast::L, regular::L; decay::Real) where {L<:dReLU}
+    @assert size(fast) == size(regular)
+    decayfast!(fast.θp, regular.θp; decay)
+    decayfast!(fast.θn, regular.θn; decay)
+    decayfast!(fast.γp, regular.γp; decay)
+    decayfast!(fast.γn, regular.γn; decay)
+end
+
+function decayfast!(ωfast::AbstractArray, ωregular::AbstractArray; decay::Real)
+    @assert size(ωfast) == size(ωregular)
+    ωfast .= decay .* ωfast + (1 - decay) .* ωregular
+end
+
+
+end # module
